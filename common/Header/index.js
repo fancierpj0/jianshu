788a84683b03aa09abe824bb2a5a5799a01e8a7a
Cris <nicai@163.com>
2018-07-13 16:21:35 +0800

    将Header组件转化为类组件

--- common/Header/index.js
+++ common/Header/index.js
@@ -4,73 +4,75 @@ import {CSSTransition} from 'react-transition-group';
 import {connect} from 'react-redux';
 import {actionCreators} from './store';
 
-const getListArea = (show) => {
-  if (show) {
-    return (
-      <SearchInfo>
-        <SearchInfoTitle>
-          热门搜索
-          <SearchInfoSwitch>换一批</SearchInfoSwitch>
-        </SearchInfoTitle>
-        <SearchInfoList>
-          <SearchInfoItem>教育</SearchInfoItem>
-          <SearchInfoItem>教育</SearchInfoItem>
-          <SearchInfoItem>教育</SearchInfoItem>
-          <SearchInfoItem>教育</SearchInfoItem>
-          <SearchInfoItem>教育</SearchInfoItem>
-          <SearchInfoItem>教育</SearchInfoItem>
-        </SearchInfoList>
-      </SearchInfo>
-    )
-  } else {
-    return null
-  }
-};
-
-export default connect(
-  //将immutable对象转换为普通对象
-  // state => state.header.toObject()
+@connect(
   state => state.get('header').toObject()
-  //会自动bindActionCreators 变成(...args) => dispatch(actions[key](...args))
   , actionCreators
-)(function ({focused, handleInputFocus, handleInputBlur}) {
-  return (
-    <HeaderWrapper>
-      <Logo/>
-      <Nav>
-        <NavItem className='left active'>首页</NavItem>
-        <NavItem className='left'>下载APP</NavItem>
-        <NavItem className='right'>登录</NavItem>
-        <NavItem className='right'>
-          <i className='iconfont'>&#xe636;</i>
-        </NavItem>
+)
+export default class Header extends React.Component {
+  getListArea = (show) => {
+    if (show) {
+      return (
+        <SearchInfo>
+          <SearchInfoTitle>
+            热门搜索
+            <SearchInfoSwitch>换一批</SearchInfoSwitch>
+          </SearchInfoTitle>
+          <SearchInfoList>
+            <SearchInfoItem>教育</SearchInfoItem>
+            <SearchInfoItem>教育</SearchInfoItem>
+            <SearchInfoItem>教育</SearchInfoItem>
+            <SearchInfoItem>教育</SearchInfoItem>
+            <SearchInfoItem>教育</SearchInfoItem>
+            <SearchInfoItem>教育</SearchInfoItem>
+          </SearchInfoList>
+        </SearchInfo>
+      )
+    } else {
+      return null
+    }
+  };
 
-        <NavSearchWrapper>
-          <CSSTransition
-            in={focused}
-            timeout={200}
-            classNames='slide'
-          >
-            <NavSearch
-              className={focused ? 'focused' : ''}
-              onFocus={handleInputFocus}
-              onBlur={handleInputBlur}
-            ></NavSearch>
-          </CSSTransition>
-          <i
-            className='iconfont'
-            className={focused ? 'focused iconfont' : 'iconfont'}
-          >&#xe614;</i>
-          {getListArea(focused)}
-        </NavSearchWrapper>
-      </Nav>
-      <Addition>
-        <Button className='writting'>
-          <i className='iconfont'>&#xe615;</i>
-          写文章
-        </Button>
-        <Button className='reg'>注册</Button>
-      </Addition>
-    </HeaderWrapper>
-  )
-});
+  render() {
+    let {focused, handleInputFocus, handleInputBlur} = this.props;
+
+    return (
+      <HeaderWrapper>
+        <Logo/>
+        <Nav>
+          <NavItem className='left active'>首页</NavItem>
+          <NavItem className='left'>下载APP</NavItem>
+          <NavItem className='right'>登录</NavItem>
+          <NavItem className='right'>
+            <i className='iconfont'>&#xe636;</i>
+          </NavItem>
+
+          <NavSearchWrapper>
+            <CSSTransition
+              in={focused}
+              timeout={200}
+              classNames='slide'
+            >
+              <NavSearch
+                className={focused ? 'focused' : ''}
+                onFocus={handleInputFocus}
+                onBlur={handleInputBlur}
+              ></NavSearch>
+            </CSSTransition>
+            <i
+              className='iconfont'
+              className={focused ? 'focused iconfont' : 'iconfont'}
+            >&#xe614;</i>
+            {this.getListArea(focused)}
+          </NavSearchWrapper>
+        </Nav>
+        <Addition>
+          <Button className='writting'>
+            <i className='iconfont'>&#xe615;</i>
+            写文章
+          </Button>
+          <Button className='reg'>注册</Button>
+        </Addition>
+      </HeaderWrapper>
+    )
+  }
+}
